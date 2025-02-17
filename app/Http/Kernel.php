f4e96c89c79d1073bf12fdd2f0f94be53e0df0ee
Samuel <daw08.2024@gmail.com>
2025-02-17 22:21:26 +0100

    Primer Commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,8 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            'throttle:api',
         ],
     ];
 
@@ -65,4 +67,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
 }
